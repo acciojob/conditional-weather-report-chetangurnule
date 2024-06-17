1e7cb8fb033e4f631153aaa124caa3af92224c10
Chetan Gurnule <105580754+chetangurnule@users.noreply.github.com>
2024-06-17 11:52:30 +0530

    Update WeatherDisplay.js

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,1 +1,15 @@
+import React from 'react';
+
+const WeatherDisplay = ({ weather }) => {
+  const { temperature, conditions } = weather;
+
+  return (
+    <div>
+      <p style={{ color: {temperature > 20 ? "red" : "blue"} }}>Temperature: {temperature}</p>
+      <p>Conditions: {conditions}</p>
+    </div>
+  );
+};
+
+export default WeatherDisplay;
 
