94e8d357716b3d7e96aea353618e91b3f5643f54
Chetan Gurnule <105580754+chetangurnule@users.noreply.github.com>
2024-06-17 11:57:47 +0530

    Update WeatherDisplay.js

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -2,13 +2,12 @@ import React from 'react';
 
 const WeatherDisplay = ({ weather }) => {
   const { temperature, conditions } = weather;
-  const temperatureColor = temperature > 20 ? 'red' : 'blue';
 
   return (
     <div>
       <p>
         Temperature:{' '}
-        <span style={{ color: temperature ? "red" : "blue" }}>{temperature}</span>
+        <span style={{ color: temperature > 20 ?  "red" : "blue" }}>{temperature}</span>
       </p>
       <p>Conditions: {conditions}</p>
     </div>
